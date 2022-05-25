97cbeb67aa13b301247479a756096ddc474f25a0
luciano <ferreyraluciano.lf@gmail.com>
2022-05-25 16:19:36 -0300

    LUCHO HACIENDO CAGADAS

---
+++ GUIA15-EJ2/src/Entidades/carpincho.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Marisel
+ */
+public class carpincho {
+    public String nombre; 
+    public String  apodo; 
+
+    public carpincho() {
+    }
+
+    public carpincho(String nombre, String apodo) {
+        this.nombre = nombre;
+        this.apodo = apodo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+    
+}
